aecaa5ad29de4e3ee2e1869a48becd0c4370a3ad
Harry <harrytbp@gmail.com>
2018-05-11 16:19:24 +0800

    Rang conversations by number of messages

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
 import json
 from pprint import pprint
+import os
+rootdir = '/Users/harrytbp/Desktop/facebook-harrybp/messages'
 
-with open('messages/harrybondpreston_3b4f5b1812/message.json') as json_data:
-    data = json.load(json_data)
-    json_data.close()
-    pprint(data)
-    print(len(data['messages']));
+people = {}
+
+for subdir, dirs, files in os.walk(rootdir):
+	for file in files:
+		if file == 'message.json':
+			filepath = os.path.join(subdir, file)
+
+			with open(filepath) as json_data:
+				data = json.load(json_data)
+				json_data.close()
+				if 'messages' in data:
+					personNameList = subdir.split('/')
+					personName = personNameList[-1]
+					people[personName] = len(data['messages'])
+
+print(sorted(people.items(), key=lambda x:x[1]))
