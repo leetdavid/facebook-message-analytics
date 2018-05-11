b6b3a3a5d90797e2bf277916a272f3cbfce7ec54
Harry <harrytbp@gmail.com>
2018-05-11 16:54:55 +0800

    Graph conversations by number messages

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import json
 from pprint import pprint
 import os
-rootdir = '/Users/harrytbp/Desktop/facebook-harrybp/messages'
+import plotly.offline as offline
+import plotly.graph_objs as go
 
-people = {}
+rootdir = '/Users/harrytbp/Desktop/facebook-harrybp'
 
-for subdir, dirs, files in os.walk(rootdir):
+people = []
+numberMessages = []
+
+#Read from files
+for subdir, dirs, files in os.walk(rootdir + '/messages'):
 	for file in files:
 		if file == 'message.json':
 			filepath = os.path.join(subdir, file)
@@ -14,8 +19,30 @@ for subdir, dirs, files in os.walk(rootdir):
 				data = json.load(json_data)
 				json_data.close()
 				if 'messages' in data:
-					personNameList = subdir.split('/')
-					personName = personNameList[-1]
-					people[personName] = len(data['messages'])
+					personName = data['title']
+					people.append(personName)
+					numberMessages.append(len(data['messages']))
 
-print(sorted(people.items(), key=lambda x:x[1]))
+#Sort
+numberMessages, people = zip(*sorted(zip(numberMessages, people), reverse=True))
+people = list(people)
+numberMessages = list(numberMessages)
+total = 0
+
+#Trim 
+filterNumber = 20
+for x in range(filterNumber, len(people)):
+	total += numberMessages[x];
+people = people[0:filterNumber]
+numberMessages = numberMessages[0:filterNumber]
+people.append('Other')
+numberMessages.append(total)
+
+#Plot
+data = [go.Bar(
+            x=people,
+            y=numberMessages
+    )]
+
+offline.plot(data, filename='basic-bar')
+
