b19bd2dc0a4062d0210ac050bcb20bce222df7fe
David Lee <leetdavid@outlook.com>
2018-05-11 18:01:43 +0800

    added chat analysis

---
+++ chat_analysis.py
@@ -0,0 +1,10 @@
+import os
+import json
+from pprint import pprint
+
+import plotly.offline as offline
+import plotly.graph_objs as go
+
+chat_dir = 'C:\\Users\\David Lee\\projects\\facebook-leetdavid\\messages\\PeytonZhang_b19cce929a\\message.json'
+
+#Read the file
