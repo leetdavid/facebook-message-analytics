6602288d67f4bc66318257d3662c49315cd47c8f
David Lee <leetdavidu@gmail.com>
2018-05-13 23:01:55 +0800

    Creates directory "/graphs/" if it does not exist

--- overall_message_stats.py
+++ overall_message_stats.py
@@ -51,6 +51,8 @@ def graph(rootdir, filename, filterGroupChats):
 		}  
 	)
 	fig = go.Figure(data=data, layout=layout)
+	if not os.path.exists('graphs/'):
+		os.makedirs('graphs/')
 	offline.plot(fig, filename='graphs/' +filename + '.html', auto_open=False, config=config)
 
 
