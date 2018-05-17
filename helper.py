e22dd38ce21f5bba49884604c7dddfce846f08f2
David Lee <leetdavid@outlook.com>
2018-05-17 17:19:50 +0800

    Added Whatsapp to Facebook-Style JSON conversion functionality

--- helper.py
+++ helper.py
@@ -1,7 +1,17 @@
+import os
+
 def hasParam(argv, howToUse = None):
     if len(argv) > 1:
-        return True
+        return len(argv)
     else:
         if howToUse:
             print(howToUse)
         return False
+
+def createDirectories(filepath):
+    if not os.path.exists(os.path.dirname(filepath)):
+        try:
+            os.makedirs(os.path.dirname(filepath))
+        except OSError as exc: #Guard Against Race Condition
+            if exc.errno != errno.EEXIST:
+                raise
